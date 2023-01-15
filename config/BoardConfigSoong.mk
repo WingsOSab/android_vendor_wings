28ed82d14a86a829272366e8314cceaf818159ea
cjh1249131356 <cjh1249131356@gmail.com>
2023-01-15 17:49:40 +0800

    vendor: soong: Add TARGET_USES_OPLUS_CAMERA
    Change-Id: Ie0fc0391a6e3fb407a158c5d8642b1b311f30af2
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -46,7 +46,8 @@ SOONG_CONFIG_wingsGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
     target_surfaceflinger_udfps_lib \
-    uses_egl_display_array
+    uses_egl_display_array \
+    uses_oplus_camera
 
 SOONG_CONFIG_NAMESPACES += wingsNvidiaVars
 SOONG_CONFIG_wingsNvidiaVars += \
@@ -75,6 +76,7 @@ SOONG_CONFIG_wingsNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_
 SOONG_CONFIG_wingsQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_wingsQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_wingsQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_wingsGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
